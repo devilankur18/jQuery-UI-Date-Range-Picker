fb2f6a50cd49e096ff7e862aa68d0bfaacff40de
addyosmani <addyosmani@gmail.com>
2011-12-06 17:20:47 +0000

    adding initial changes to apply bootstrap theme

--- js/daterangepicker.jQuery.js
+++ js/daterangepicker.jQuery.js
@@ -325,8 +325,8 @@ jQuery.fn.daterangepicker = function(settings){
 
 	//add arrows (only available on one input)
 	if(options.arrows && rangeInput.size()==1){
-		var prevLink = jQuery('<a href="#" class="ui-daterangepicker-prev ui-corner-all" title="'+ options.prevLinkText +'"><span class="ui-icon ui-icon-circle-triangle-w">'+ options.prevLinkText +'</span></a>');
-		var nextLink = jQuery('<a href="#" class="ui-daterangepicker-next ui-corner-all" title="'+ options.nextLinkText +'"><span class="ui-icon ui-icon-circle-triangle-e">'+ options.nextLinkText +'</span></a>');
+		var prevLink = jQuery('<a href="#" class="ui-daterangepicker-prev ui-corner-all" title="'+ options.prevLinkText +'"><span class="ui-icon ui-icon-triangle-1-a">'+ options.prevLinkText +'</span></a>');
+		var nextLink = jQuery('<a href="#" class="ui-daterangepicker-next ui-corner-all" title="'+ options.nextLinkText +'"><span class="ui-icon ui-icon-triangle-1-e">'+ options.nextLinkText +'</span></a>');
 	
 		jQuery(this)
 		.addClass('ui-rangepicker-input ui-widget-content')
